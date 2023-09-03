821a6b7caf73f3383f4af0a47e34e9e1b16d5a48
JaiRajGunnu <jairajgsklm@gmail.com>
2023-09-03 18:01:53 +0530

    js

---
+++ main.js
@@ -0,0 +1,7 @@
+let result = document.getElementById("result");
+let searchBtn = document.getElementById("search-btn");
+let cancelBtn = document.getElementById("cancel-btn");
+let userInput = document.getElementById("user-inp");
+let appId = "YOUR_API_ID";
+let appKey = "YOUR_API_KEY";
+
