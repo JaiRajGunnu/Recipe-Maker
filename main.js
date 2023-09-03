6548c68fb0dbcb65257d5114337000050d48edf1
JaiRajGunnu <jairajgsklm@gmail.com>
2023-09-03 18:06:05 +0530

    recp map

--- main.js
+++ main.js
@@ -44,5 +44,19 @@ function clearSearchInput() {
         let recipes = data.hits;
         if (recipes.length === 0) {
           result.innerHTML = `<h3 style="color:red; margin:10px 5px;font-size:120%;">Sorry, No recipes found.</h3>`;
-        }
-        
+        } else {
+          let recipeCards = recipes
+            .map((recipe, index) => {
+              let recipeData = recipe.recipe;
+              return `
+                <div class="recipe-card">
+                  <img class="recp-img" src="${recipeData.image}" alt="${recipeData.label}">
+                  <div class="recipe-info">
+                    <h2>${recipeData.label}</h2>
+                    <p>Source: ${recipeData.source}</p>
+                    <button class="view-recipe-btn" data-uri="${recipeData.uri}">View Recipe <i class="fa-solid fa-arrow-right" style="margin-left:04px; transform: rotateZ(330deg); "></i></button>
+                  </div>
+                </div>
+              `;
+            })
+      
